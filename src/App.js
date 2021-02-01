76c12c7253c7f43f1db461edf4f56329847d12ac
Cem  Onal <42888722+8mecem8@users.noreply.github.com>
2021-02-01 13:13:48 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import {voteUpOf} from './reducers/anecdoteReducer'
+
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+  const anecSorted = anecdotes.sort((at, ut) => ut.votes - at.votes)
+
   const dispatch = useDispatch()
 
+
+
   const vote = (id) => {
-    console.log('vote', id)
+    
+          dispatch(voteUpOf(id))
+  }
+
+
+  const add = (e) => {
+      e.preventDefault()
+
+console.log("e.target.addContent.value is",e.target.addContent.value)
+
+      dispatch({
+        type: 'ADDANEC',
+        data: e.target.addContent.value
+      })
   }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecSorted.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
@@ -24,8 +43,8 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={add}>
+        <div><input name="addContent" /></div>
         <button>create</button>
       </form>
     </div>
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -19,11 +19,59 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
 
+
+
+
+
+
+export const voteUpOf = (id) => {
+  return {
+      type: 'VOTEUP',
+      data: id
+
+  }
+
+}
+
+
+
+
+
+
+const reducer = (state = initialState, action) => {
+  //console.log('state now: ', Object.values(state))
+ // console.log('action', action.id)
+
+  switch(action.type){
+
+    case 'VOTEUP':
+       let fAnec = state.find(at =>  at.id === action.data)
+         fAnec = {
+          ...fAnec,
+           votes: fAnec.votes + 1
+         }                                                                                        // content: fAnec.content,id: fAnec.id,
+
+       return state.map(at => at.id !== action.data ? at : fAnec)
+     case 'ADDANEC':
+
+let att = getId()
+
+         
+      let  addNewCon = {
+          content: action.data,
+          id:  att  === state.map(at => at.id) ? getId() : att ,
+          votes: 0
+        }
+console.log(addNewCon)
+       return state.concat(addNewCon)
+    default:
   return state
+  }
 }
 
 export default reducer
+
+
+
+//Object.values(state)
